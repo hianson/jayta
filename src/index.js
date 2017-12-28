d062bfe981bade1e80b3bf1004a755dad631e22b
Anson <leungans@gmail.com>
2017-12-27 17:44:59 -0800

    add bootstrap css, js, jq, etc

--- src/index.js
+++ src/index.js
@@ -2,12 +2,32 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import YTSearch from 'youtube-api-search';
+
+const API_KEY = 'AIzaSyDS7Tzkft4cV7YtXYdbdNTKi6enI_CtIHM';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: []
+    }
+
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos })
+    })
+
+
+  }
+
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
